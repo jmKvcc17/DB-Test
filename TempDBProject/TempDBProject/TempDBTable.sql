0e5b6722e0aa00cf90a9b398ebd2f60512a4065a
jmKvcc17 <jmeachum17@gmail.com>
2017-05-11 10:46:44 -0500

    Add files via upload

---
+++ TempDBProject/TempDBProject/TempDBTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TempDBTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [TempCelcius] NCHAR(10) NULL, 
+    [TempFahrenheit] NCHAR(10) NULL, 
+    [Time] TIME NULL, 
+    [Date] DATE NULL
+)
